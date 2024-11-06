0abe14162d4431832729b5d12e1a94bad6bda658
23020660 Nguyễn Văn Hoàng Hải <23020660@vnu.edu.vn>
2024-11-06 10:46:51 +0700

    delete unused library

--- firstProject/src/main/classes/MyQr.java
+++ firstProject/src/main/classes/MyQr.java
@@ -15,6 +15,7 @@ import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 public class MyQr {
     public static final int QR_IMAGE_HEIGHT = 200;
     public static final int QR_IMAGE_WIDTH = 200;
+
     public static String fullFillInformationQR(Document document) {
         String fullFillInformation = "ISBN: " + document.getDocumentISBN();
 
@@ -39,10 +40,9 @@ public class MyQr {
 
     // Function to create the QR code
     public static void createQR(String data, String path,
-                                String charset, Map hashMap,
-                                int height, int width)
-            throws WriterException, IOException
-    {
+            String charset, Map hashMap,
+            int height, int width)
+            throws WriterException, IOException {
 
         BitMatrix matrix = new MultiFormatWriter().encode(
                 new String(data.getBytes(charset), charset),
@@ -53,5 +53,29 @@ public class MyQr {
                 path.substring(path.lastIndexOf('.') + 1),
                 new File(path));
     }
-}
 
+    public static void main(String[] args)
+            throws WriterException, IOException,
+            NotFoundException {
+
+        // The data that the QR code will contain
+        String data = "www.geeksforgeeks.org";
+
+        // The path where the image will get saved
+        String path = "demo.png";
+
+        // Encoding charset
+        String charset = "UTF-8";
+
+        Map<EncodeHintType, ErrorCorrectionLevel> hashMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
+
+        hashMap.put(EncodeHintType.ERROR_CORRECTION,
+                ErrorCorrectionLevel.L);
+
+        // Create the QR code and save
+        // in the specified folder
+        // as a jpg file
+        createQR(data, path, charset, hashMap, 200, 200);
+        System.out.println("QR Code Generated!!! ");
+    }
+}
